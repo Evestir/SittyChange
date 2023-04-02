7de9f05144d302d7cff4a935b5f6a28e98eb7d29
Evestir <54894315+Evestir@users.noreply.github.com>
2023-04-03 06:33:44 +0900

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 #include <list>
 #include <algorithm>
+#include <random>
+#include <unordered_set>
 
 using namespace std;
+
+std::list<int> numbers;
+
+bool deterInt(int num){
+    int i = 0;
+    
+    while (i < numbers.size()){
+        auto cnm = std::next(numbers.begin(), i);
+        if (num == *cnm){
+            return true;
+        }
+        
+        i++;
+    }
+    
+    return false;
+}
 
 int main() {
     int hang;
     int yeol;
     int randomNumber;
-    int nigga = 0;
-    int niggano = 1;
+    int pup = 0;
+    int coc = 1;
     
     cout << "[>] 행을 입력해 주세요: ";
     cin >> hang;
@@ -18,33 +37,38 @@ int main() {
     cin >> yeol;
     
     cout << "[>] 학생 수를 입력해 주세요: ";
-    cin >> nigga;
+    cin >> pup;
     
     cout << "\n";
     
-    std::list<int> numbers;
+    std::list<int> blacklist = { 23 };
+    std::list<int> whitelist = { 32, 35 };
+            
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, pup);
     
-    while (niggano != nigga){
-        randomNumber = rand() % nigga + 1;
-        auto iter = std::find(numbers.begin(), numbers.end(), randomNumber);
-        if (!(iter != numbers.end())){
+    while (coc <= pup){
+        randomNumber = dis(gen);
+        
+        if (deterInt(randomNumber) == false){
+            cout << "Added " << randomNumber << " to the list. \n";
             numbers.push_back(randomNumber);
-            niggano += 1;
+            coc++;
         }
     }
     
-    niggano = 0;
+    coc = 0;
     
-    cout << "[*] 설정된 학생 수: " << nigga << "\n\n";
-        
+    cout << "[*] 설정된 학생 수: " << pup << "\n\n";
+            
     for (uint n = 1; n <= yeol; n++){
         cout << "[" << n << "] ";
         for (uint i = 1; i <= hang; i++){
-            if (niggano == nigga){
+            if (coc == pup){
                 break;
             }
-            auto cnm = std::next(numbers.begin(), niggano);
-            
+            auto cnm = std::next(numbers.begin(), coc);
             if (i < hang){
                 if (*cnm < 10){
                     cout << *cnm << "   ";
@@ -57,10 +81,11 @@ int main() {
                 cout << *cnm << "\n";
             }
             
-            niggano += 1;
+            coc++;
         }
     }
-    cout << "\n";
+    
+    cout << '\n';
     
     return (0);
 }
